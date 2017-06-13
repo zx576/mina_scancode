073afb9323085e8ce99b8e4e9542b578dd0a1d7f
zx576 <782744680@qq.com>
2017-06-13 12:58:05 +0800

    '修改admin显示'

--- mycode/admin.py
+++ mycode/admin.py
@@ -12,17 +12,17 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Goods)
 class GoodsAdmin(admin.ModelAdmin):
     list_display = ('name', 'belong', 'code', 'count', 'remarks', 'created_time', 'last_active_time')
-    list_filter = ('belong__name',)
+    # list_filter = ('belong__name',)
 
 @admin.register(Directory)
 class DirAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'freq', 'created_time', 'last_active_time')
-    list_filter = ('owner__nickname',)
+    # list_filter = ('owner__nickname',)
 
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     readonly_fields = ('created_time',)
-    list_filter = ('owner__nickname', 'type', 'dir__name', 'good__name')
+    list_filter = ('type',)
     list_display = ('owner', 'created_time', 'type', 'dir', 'good')
 
